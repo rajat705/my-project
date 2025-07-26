39fe86812fb32b13a7036acf1b53532a7dac7098
Rajat Nagar <rajatnagar7893@gmail.com>
2025-07-26 12:09:07 +0530

    background2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import "./styles/Header.css"; // Ensure CSS is applied
+import "./styles/Header.css";
 import Hero from "./components/Hero";
 import Connect from "./components/Connect";
 import Footer from "./components/Footer";
@@ -14,16 +14,73 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Hero />
-      {/* <About /> */}
-      <Education />
-      <Experience />
-      <Projects />
-      <Certification />
-      {/* <Connect /> */}
-      <Footer />
+
+      <section id="home">
+        <Hero />
+      </section>
+
+      {/* Uncomment if you're using About */}
+      {/* <section id="about">
+        <About />
+      </section> */}
+
+      <section id="edu">
+        <Education />
+      </section>
+
+      <section id="exp">
+        <Experience />
+      </section>
+
+      <section id="project">
+        <Projects />
+      </section>
+
+      <section id="certificates">
+        <Certification />
+      </section>
+
+      {/* Uncomment if you're using Connect */}
+      {/* <section id="connect">
+        <Connect />
+      </section> */}
+
+      <section id="contact">
+        <Footer />
+      </section>
     </div>
   );
 }
 
 export default App;
+
+
+// import React from "react";
+// import Header from "./components/Header";
+// import "./styles/Header.css"; // Ensure CSS is applied
+// import Hero from "./components/Hero";
+// import Connect from "./components/Connect";
+// import Footer from "./components/Footer";
+// import Projects from "./components/Projects";
+// import Certification from "./components/Certification";
+// import Experience from "./components/Experience";
+// import About from "./components/About";
+// import Education from "./components/Education";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Hero />
+//       {/* <About /> */}
+//       <Education />
+//       <Experience />
+//       <Projects />
+//       <Certification />
+//       {/* <Connect /> */}
+//       <Footer />
+//     </div>
+//   );
+// }
+
+// export default App;
